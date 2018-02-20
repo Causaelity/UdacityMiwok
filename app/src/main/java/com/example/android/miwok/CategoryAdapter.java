8826cb530e0056b0e8ca07915d29e62f22162e74
Causaelity <Tiberius@Faith.local>
2018-02-19 23:27:27 -0800

    Complete with Fragments

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,5 +1,6 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -10,7 +11,13 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fm) { super(fm); }
+    /** Context of the app */
+    private Context mContext;
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
 
     @Override
     public Fragment getItem(int position) {
@@ -29,5 +36,22 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.category_numbers);
+            case 1:
+                return mContext.getString(R.string.category_family);
+            case 2:
+                return mContext.getString(R.string.category_colors);
+            default:
+                return mContext.getString(R.string.category_phrases);
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,52 +14,23 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-//        TextView numbers = findViewById(R.id.numbers);
-//        numbers.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-//                startActivity(numbersIntent);
-//            }
-//        });
-//
-//        TextView colors = findViewById(R.id.colors);
-//        colors.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent colorIntent = new Intent(MainActivity.this, ColorsActivity.class);
-//                startActivity(colorIntent);
-//            }
-//        });
-//
-//        TextView family = findViewById(R.id.family);
-//        family.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-//                startActivity(familyIntent);
-//            }
-//        });
-//
-//        TextView phrases = findViewById(R.id.phrases);
-//        phrases.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent phraseIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-//                startActivity(phraseIntent);
-//            }
-//        });
-
         // Find the view page that will allow the user to swipe between fragments
         ViewPager viewPager = findViewById(R.id.viewpager);
 
         // Create an adapter that knows which fragment should be shown on each page
-        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
         // Set the adapter onto the new pager
         viewPager.setAdapter(adapter);
 
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = findViewById(R.id.tabs);
 
+        // Connect the tab layout with the view pager. This will
+        // 1. Update the tab layout when the view pager is swiped
+        // 2. Update the view pager when a tab is selected
+        // 3. Set the tab layout's tab names with the view pager's adapter's titles by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
 
 
     }
