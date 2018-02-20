8b5b5bc720adc81b80053a356ea91bda4f288fd3
Causaelity <Tiberius@Faith.local>
2018-02-19 22:29:09 -0800

    Fragments added pre delete

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,42 +13,51 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
+//        TextView numbers = findViewById(R.id.numbers);
+//        numbers.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+//                startActivity(numbersIntent);
+//            }
+//        });
+//
+//        TextView colors = findViewById(R.id.colors);
+//        colors.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent colorIntent = new Intent(MainActivity.this, ColorsActivity.class);
+//                startActivity(colorIntent);
+//            }
+//        });
+//
+//        TextView family = findViewById(R.id.family);
+//        family.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+//                startActivity(familyIntent);
+//            }
+//        });
+//
+//        TextView phrases = findViewById(R.id.phrases);
+//        phrases.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent phraseIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+//                startActivity(phraseIntent);
+//            }
+//        });
 
-        TextView numbers = findViewById(R.id.numbers);
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        // Find the view page that will allow the user to swipe between fragments
+        ViewPager viewPager = findViewById(R.id.viewpager);
 
-        TextView colors = findViewById(R.id.colors);
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent colorIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(colorIntent);
-            }
-        });
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
 
-        TextView family = findViewById(R.id.family);
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
+        // Set the adapter onto the new pager
+        viewPager.setAdapter(adapter);
 
-        TextView phrases = findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phraseIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phraseIntent);
-            }
-        });
 
 
 
